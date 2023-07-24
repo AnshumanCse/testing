d2e31ba4c9e122447e54f80ec0d647023e436e72
AnshumanCse <anshumantiwari.cse@gmail.com>
2023-07-24 12:58:41 +0530

    testing

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv"
+
+const app = express();
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send( "<h1>Hellow this is node js </h1>"
+  );
+});
+const PORT = process.env.PORT ;
+app.listen(PORT, (req, res) => {
+  console.log(`app running on ${PORT} successfully:`);
+});
