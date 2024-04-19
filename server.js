fbffa7b39e91b630c5589af620c2f3bf52e85705
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2024-04-19 11:59:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send( "<h1>Hellow this is node js Deployed successfully</h1>"
   );
 });
+app.get("/testing", (req, res) => {
+  res.send( "<h1>Server on testing mode</h1>"
+  );
+});
 const PORT = process.env.PORT ;
 app.listen(PORT, (req, res) => {
   console.log(`app running on ${PORT} successfully:`);
